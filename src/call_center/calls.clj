e1a29685372b14f4c630ea5f4713939595f905db
Karthikeyan <karthikeyan@bonfleet.com>
2019-11-26 22:53:15 +0530

    Make a new chan for the employees
    - what:
       - When starting the calls, add employees to the channel

--- src/call_center/calls.clj
+++ src/call_center/calls.clj
@@ -3,7 +3,10 @@
             [clojure.tools.logging :as log]))
 
 
+(def employees-count 5)
 (def incoming-call-chan (chan))
+(def employees-chan (chan employees-count))
+(def employees ["naresh" "suresh" "priya" "raju" "karthik"])
 
 (defn start-listening-to-calls
   []
@@ -11,6 +14,14 @@
         (let [new-caller-phone (<! incoming-call-chan)]
           (log/infof "New incoming phone call from phone:%s" new-caller-phone)))))
 
-(defn stop-listening-to-calls
+(defn start-calls-handler
   []
+  (go (doseq [employee employees]
+        (>! employees-chan employee)))
+  (start-listening-to-calls)
+  (log/info "Listening to calls now!"))
+
+(defn stop-calls-handler
+  []
+  (close! employees-count)
   (close! incoming-call-chan))
